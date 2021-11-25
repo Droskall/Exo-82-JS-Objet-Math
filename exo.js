b861f7e87ccefecd99f7187e6e364113e80100d3
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-11-25 15:08:30 +0100

    finish

---
+++ exo.js
@@ -0,0 +1,41 @@
+// 1
+console.log("34.589456 ==> " + Math.floor(34.589456));
+
+//2
+console.log("68.412784 ==> " + Math.floor(68.412784));
+
+//3
+console.log("39.456 ==> " + Math.ceil(39.456));
+
+//4
+console.log("27.34876541 ==> " + Math.ceil(27.34876541));
+
+//5
+console.log("5.68 ==> " + Math.round(5.68));
+
+//6
+console.log("5.41 ==> " + Math.round(5.41));
+
+//7
+console.log("6.12457 ==> " + Math.trunc(6.12457));
+
+//8
+console.log("9.568124 ==> " + Math.trunc(9.568124));
+
+//9
+console.log("Valeur minimal de 5, 1, 12, -1, 45 ==> " + Math.min(5, 1, 12, -1, 45));
+
+//10
+console.log("Valeur minimal de 9, 12, 16, 44, 3 ==> " + Math.min(9, 12, 16, 44, 3));
+
+//11
+console.log("Valeur minimal de 5, 1, 12, -1, 45 ==> " + Math.max(5, 1, 12, -1, 45));
+
+//12
+console.log("Valeur minimal de 9, 12, 16, 44, 3 ==> " + Math.max(9, 12, 16, 44, 3));
+
+//13
+console.log("Valeur absolue de -6 ==> " + Math.abs(-6));
+
+//14
+console.log("Valeur absolue de -56 ==> " + Math.abs(-56));
